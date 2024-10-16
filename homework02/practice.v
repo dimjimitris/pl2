c01640ca43cc7bc9f23e7159bf1315c4ff589d74
dimjimitris <dimitrisgeor01@gmail.com>
2024-10-16 16:54:23 +0300

    finished practice

--- homework02/practice.v
+++ homework02/practice.v
@@ -9,7 +9,11 @@ Require Import Coq.Lists.List Coq.Init.Nat Coq.Arith.Arith Lia.
 Lemma or_comut :
   forall P Q, P \/ Q -> Q \/ P.
 Proof.
-Admitted.
+  intros P Q HPQ.
+  inversion HPQ as [HP | HQ].
+  - right. apply HP.
+  - left. apply HQ.
+Qed.  
 
 (** Law of contrapositive.  *)
 
@@ -17,7 +21,13 @@ Lemma contrapositive1 :
   forall P Q,
     (P -> Q) -> ~Q -> ~P. 
 Proof.
-Admitted.
+  intros P Q H HNQ. 
+  intros HP.
+  apply HNQ. apply H. apply HP.
+Qed.
+  
+
+
 
 
 Definition EM := forall P, P \/ ~ P.
@@ -30,14 +40,41 @@ Lemma contrapositive2 :
   forall P Q,
     (~Q -> ~P) -> P -> Q. 
 Proof.
-Admitted.
+  intros HEM P Q HN HP.
+  unfold EM in HEM.
+  specialize HEM with (P := Q).
+  inversion HEM as [HQ | HNQ].
+  - apply HQ.
+  - apply HN in HNQ. contradiction.
+Qed.
+
+
+
+Print and.
 
 (** Distributive property of conjunction over disjuntion. *)
 Lemma and_distr :
   forall P Q R, 
     (P \/ Q) /\ R <-> P /\ R \/ Q /\ R. 
 Proof.                         
-Admitted.
+  intros P Q R. split.
+  {
+    intros H.
+    destruct H as [[Hp | Hq] Hr].
+    - left. constructor; assumption.
+    - right. constructor; assumption.
+  }
+  {
+    intros H.
+    destruct H as [[Hp Hr] | [Hq Hr]].
+    - split.
+      + left. assumption.
+      + assumption.
+    - split.
+      + right. assumption.
+      + assumption.  
+  }
+Qed.
 
 
 (** ** Natural Numbers *)
@@ -68,13 +105,21 @@ Admitted.
    fragment of logic, [lia] is not guaranteed to succeed if the goal
    is true. *)
 
-
 Lemma strong_nat_ind :
   forall (P : nat -> Prop),
     (forall n, (forall m, m < n -> P m) -> P n) ->
     forall n, P n.
 Proof.
-Admitted.
+  intros P Hind n.
+
+  assert (H: forall m, m <= n -> P m).
+  {
+    induction n as [| n' IHn']; intros m H; apply Hind.
+    - intros. lia.
+    - intros. apply IHn'. lia. 
+  }
+  apply H. lia.
+Qed.
 
 
 (** Associativity of addition *)
@@ -83,8 +128,11 @@ Lemma add_assoc:
   forall n m k,
     n + m + k = n + (m + k). 
 Proof.
-Admitted.
-
+  intros n m k.
+  induction n.
+  - simpl. reflexivity.
+  - simpl. rewrite IHn. reflexivity. 
+Qed.
 
 (** Distributive property of multiplication over addition *)
 
@@ -92,24 +140,46 @@ Lemma mul_distr:
   forall n m k,
     n * (m + k) = n * m + n * k. 
 Proof.
-Admitted.
+  intros n m k.
+  induction n.
+  - simpl. reflexivity.
+  - simpl. rewrite IHn.
+    rewrite Nat.add_comm with (n := n * m).
+    rewrite  add_assoc with (m := n * m).
+    rewrite Nat.add_comm with (n := n * m).
+    rewrite add_assoc with (n := m).
+    rewrite <- add_assoc with (n := k).
+    reflexivity.
+Qed.
 
 (** Write an inductive relation [le : nat -> nat -> Prop] that is true
     if and only iff the first natural number is less than or equal to
     the second. *)
 
 Inductive le : nat -> nat -> Prop :=
-(* __ FILL IN HERE __ *). 
+  | le0: forall m, le 0 m
+  | leS: forall n m, le n m -> le (S n) (S m).
 
 Example leq_3_5:
   le 3 5.
-Proof.  
-Admitted.
+Proof.
+  right.
+  right.
+  right.
+  left.
+Qed.  
+
 
 Example not_leq_5_2:
   ~ le 5 3.
 Proof.
-Admitted.
+  intros H.
+  inversion H as [ | n m H' Hn Hm]. subst.
+  inversion H' as [ | n m H'' Hn Hm]. subst.
+  inversion H'' as [ | n m H''' Hn Hm]. subst.
+  inversion H''' as [ | n m H'''' Hn Hm].
+Qed.
+
 
 (** Note: this is exactly the definition of [<=] in the standard
     library. *)
@@ -120,7 +190,21 @@ Lemma le_transitive :
     le m k ->
     le n k.
 Proof.
-Admitted.
+  intros n.
+  induction n as [ | n' Ihn'].
+  - intros m k H1 H2.
+    constructor.
+    
+  - intros m k H1 H2.
+    inversion H1 as [ | n'' m' H3 H4 H5]. subst.
+    inversion H2 as [ | m'' k' H4 H5 H6]. subst.
+    right.
+    specialize Ihn' with (m := m') (k := k').
+    apply Ihn'; assumption.
+Qed.
+
+
+
 
 (** ** Lists *) 
 
@@ -137,8 +221,11 @@ Lemma app_nil :
   forall (A : Type) (l : list A),
     l ++ [] = l.
 Proof.
-Admitted.
-  
+  intros A l.
+  induction l as [ | x l' Ihl'].
+  - simpl. reflexivity.
+  - simpl. rewrite Ihl'. reflexivity.
+Qed.
 
 (** [rev] distributes over [app]. You will need [app_nil] and the
     lemma [app_assoc] from the standard library. *)
@@ -147,12 +234,21 @@ Lemma rev_app_distr :
   forall (A : Type) (l1 l2 : list A),
     rev (l1 ++ l2) = rev l2 ++ rev l1.
 Proof.
-Admitted.
-  
+  intros A l1 l2.
+  induction l1 as [ | x l' IHl'].
+  - simpl. rewrite app_nil. reflexivity.
+  - simpl. rewrite IHl'. rewrite <- app_assoc. reflexivity.
+Qed.
+
+
 (** [rev] is involutive. You will need [rev_app_distr] *)
 
 Lemma rev_rev_same :
   forall A (l : list A),
     rev (rev l) = l.
 Proof.
-Admitted.
+  intros A l.
+  induction l as [ | x l' IHl'].
+  - simpl. reflexivity.
+  - simpl. rewrite rev_app_distr. rewrite IHl'. simpl. reflexivity.
+Qed.
