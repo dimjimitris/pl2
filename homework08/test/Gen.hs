af22ce9b3911a6597feee5042c609119ab6152d6
dimjimitris <dimitrisgeor01@gmail.com>
2025-02-17 17:10:45 +0200

    finished hw8

--- homework08/test/Gen.hs
+++ homework08/test/Gen.hs
@@ -83,7 +83,238 @@ genTExpSize :: M.Map Type [String]  -- a map from types to variables with the co
             -> Type                 -- the type of the generated terms
             -> Int                  -- The size of the term.
             -> Gen Exp
-genTExpSize = error "Implement me!"
+genTExpSize env next t sz =
+  case t of
+    TUnit ->
+      frequency $ (6, genLit) : if sz <= 0 then [] else [
+                  (2, genApp),
+                  (2, genFst),
+                  (2, genSnd),
+                  (2, genCase),
+                  (2, genCaseL),
+                  (2, genLet),
+                  (2, genITE),
+                  (1, genLetRec)
+                ]
+                ++ zip [1..] genVar'
+
+    TInt ->
+      frequency $ (6, genLit) : if sz <= 0 then [] else [
+                  (4, genBop'),
+                  (3, genApp),
+                  (3, genCase),
+                  (3, genCaseL),
+                  (3, genLet),
+                  (2, genITE),
+                  (2, genFst),
+                  (2, genSnd),
+                  (1, genLetRec)
+                ]
+                ++ zip [1..] genVar'
+
+    TBool ->
+      frequency $ (6, genLit) : if sz <= 0 then [] else [
+                  (4, genBop'),
+                  (4, genUop),
+                  (3, genApp),
+                  (3, genCase),
+                  (3, genCaseL),
+                  (3, genLet),
+                  (2, genFst),
+                  (2, genSnd),
+                  (2, genITE),
+                  (1, genLetRec)
+                ]
+                ++ zip [1..] genVar'
+
+    TArrow t1 t2 ->
+      frequency $ (6, genAbs t1 t2) : if sz <= 0 then [] else [
+                  (3, genLet),
+                  (3, genApp),
+                  (2, genFst),
+                  (2, genSnd),
+                  (2, genITE),
+                  (2, genCase),
+                  (2, genCaseL),
+                  (1, genLetRec)
+                ]
+                ++ zip [1..] genVar'
+
+    TProd t1 t2 ->
+      frequency $ (6, genPair t1 t2) : if sz <= 0 then [] else [
+                  (3, genLet),
+                  (3, genApp),
+                  (3, genITE),
+                  (3, genCase),
+                  (3, genCaseL),
+                  (2, genFst),
+                  (2, genSnd),
+                  (1, genLetRec)
+                ]
+                ++ zip [1..] genVar'
+
+    TSum t1 t2 ->
+      frequency $ [ (6, genInl t1),
+                    (6, genInr t2) ] ++ if sz <= 0 then [] else [
+                    (3, genLet),
+                    (3, genApp),
+                    (3, genITE),
+                    (3, genCase),
+                    (3, genCaseL),
+                    (2, genFst),
+                    (2, genSnd),
+                    (1, genLetRec)
+                  ]
+                  ++ zip [1..] genVar'
+
+    TList t' ->
+      frequency $ (6, genNil) : if sz <= 0 then [] else [
+                  (4, genCons t'),
+                  (3, genLet),
+                  (3, genApp),
+                  (3, genITE),
+                  (3, genCase),
+                  (3, genCaseL),
+                  (2, genFst),
+                  (2, genSnd),
+                  (1, genLetRec)
+                ]
+                ++ zip [1..] genVar'
+
+    TVar _ -> error "genTExpSize: encountered type variable"
+  where
+    -- var
+    genVar' = case M.lookup t env of
+      Just xs -> [elements (fmap (Var nowhere) xs)]
+      Nothing -> []
+
+    -- application
+    genApp = do
+      t1 <- genType
+      e1 <- genTExpSize env next (TArrow t1 t) (sz - 1)
+      e2 <- genTExpSize env next t1 (sz - 1)
+      return $ App nowhere e1 e2
+
+    -- abstraction
+    genAbs t1 t2 = do
+      let name = "x_" ++ show next
+      let env' = addVar name t1 env
+      body <- genTExpSize env' (next + 1) t2 (sz - 1)
+      -- ! maybe just
+      return $ Abs nowhere name Nothing Nothing body
+
+    -- unit, integers, booleans
+    genLit =
+      case t of
+        TUnit -> return (Unit nowhere) -- consider units to be literals for simplicity
+        TInt -> liftM (NumLit nowhere) arbitrary
+        TBool -> liftM (BoolLit nowhere) arbitrary
+        _ -> error $ "genLit: invalid literal type: " ++ show t
+
+    -- if-then-else
+    genITE = do
+      e1 <- genTExpSize env next TBool (sz - 1)
+      e2 <- genTExpSize env next t (sz - 1)
+      e3 <- genTExpSize env next t (sz - 1)
+      return $ ITE nowhere e1 e2 e3
+
+    -- arithmetic bops, logical bops, comparison bops
+    genBop' =
+      case t of
+        TInt -> do
+          op <- intIntBop
+          e1 <- genTExpS TInt
+          e2 <- if op == Div then genNonZeroTInt else genTExpS TInt
+          return $ Bop nowhere op e1 e2
+        TBool -> oneof [liftM3 (Bop nowhere) boolBoolBop (genTExpS TBool) (genTExpS TBool),
+                        liftM3 (Bop nowhere) intBoolBop (genTExpS TInt) (genTExpS TInt)]
+        _ -> error $ "genBop': invalid binary operator type: " ++ show t
+      where
+        intIntBop = elements [Plus, Minus, Mult, Div]
+        boolBoolBop = elements [And, Or]
+        intBoolBop = elements [Lt, Gt, Le, Ge, Eq]
+        -- second expression in division will always an int literal, but that's fine
+        genNonZeroTInt = do
+          n <- arbitrary
+          if n == 0 then genNonZeroTInt else return $ NumLit nowhere n
+
+    -- boolean uops
+    genUop = liftM (Uop nowhere Not) (genTExpS TBool)
+
+    -- pair
+    genPair t1 t2 = liftM2 (Pair nowhere) (genTExpS t1) (genTExpS t2)
+
+    -- fst
+    genFst = do
+      t2 <- genType
+      e <- genTExpS (TProd t t2)
+      return $ Fst nowhere e
+
+    -- snd
+    genSnd = do
+      t1 <- genType
+      e <- genTExpS (TProd t1 t)
+      return $ Snd nowhere e
+
+    -- inl
+    genInl t' = liftM (Inl nowhere) (genTExpS t')
+
+    -- inr
+    genInr t' = liftM (Inr nowhere) (genTExpS t')
+
+    -- case
+    genCase = do
+      t1 <- genType
+      t2 <- genType
+      e <- genTExpS (TSum t1 t2)
+      let name1 = "y1_" ++ show next
+      let name2 = "y2_" ++ show next
+      let env1 = addVar name1 t1 env
+      let env2 = addVar name2 t2 env
+      e1 <- genTExpSize env1 (next + 1) t (sz - 1)
+      e2 <- genTExpSize env2 (next + 1) t (sz - 1)
+      return $ Case nowhere e name1 e1 name2 e2
+
+    -- caseL (case e of Nil -> e1 | Cons x xs -> e2)
+    genCaseL = do
+      t' <- genType
+      e <- genTExpS (TList t')
+      let name_hd = "y1_" ++ show next
+      let name_tl = "y2_" ++ show next
+      let env' = addVar name_tl (TList t') (addVar name_hd t' env)
+      e1 <- genTExpSize env (next + 1) t (sz - 1)
+      e2 <- genTExpSize env' (next + 1) t (sz - 1)
+      return $ CaseL nowhere e e1 name_hd name_tl e2
+
+    -- let
+    genLet = do
+      let name = "x_" ++ show next
+      t' <- genType
+      e <- genTExpS t'
+      let env' = addVar name t' env
+      rest <- genTExpSize env' (next + 1) t (sz - 1)
+      return $ Let nowhere name Nothing e rest
+
+    -- let rec
+    genLetRec = do
+      let xname = "x_" ++ show next
+      xt <- genType
+      let fname = "f_" ++ show next
+      let env' = addVar fname (TArrow xt t) env
+      body <- genTExpSize (addVar xname xt env') (next + 1) t (sz - 1)
+      rest <- genTExpSize env' (next + 1) t (sz - 1)
+      return $ LetRec nowhere fname xname Nothing Nothing body rest
+
+    genNil = return $ Nil nowhere
+
+    genCons t' = do
+      e1 <- genTExpS t'
+      e2 <- genTExpS (TList t')
+      return $ Cons nowhere e1 e2
+
+    -- helper functions
+    genTExpS t' = genTExpSize env next t' (sz - 1)
+    addVar x typ = M.insertWith (++) typ [x] -- (returns a function that takes an environment and adds a variable to it)
   
 
 -- Top-level function for generating well-typed expressions. You may tweak them
--- homework08/test/Main.hs
+++ homework08/test/Main.hs
@@ -4,16 +4,23 @@ import Test.Tasty
 import Test.Tasty.QuickCheck
 
 import MiniML
+import Data.Proxy
 import Gen
+
+import Test.Tasty.Ingredients
+import Test.Tasty.Options
+import Test.Tasty.Runners
 
 -- The main testing function. Runs a series of tests. Add as many additional
 -- tests as you want.
 
+ingredients :: [Ingredient]
+ingredients = [consoleTestReporter, includingOptions [Option (Proxy :: Proxy QuickCheckTests)]]
+
 main :: IO ()
-main = defaultMain $ testGroup "act"
-  [ testProperty "Parser round trip" parserRoundTrip
-  , testProperty "Type inference" testTypeInf
-  , testProperty "Placeholder" someProperty ]
+main = defaultMainWithIngredients ingredients $ localOption (QuickCheckTests 10000) $ testGroup "act"
+  [ --testProperty "Parser round trip" parserRoundTrip, 
+  testProperty "Type inference" testTypeInf ]
 
 -- A property that for a randomly MiniML as pretty-printing it and parsing it
 -- produces the original program (i.e., `parse . lex . showExp == id`)
@@ -27,9 +34,13 @@ parserRoundTrip =
 
 
 testTypeInf :: Property
-testTypeInf = counterexample "FILL IN HERE!" False
-
-
-someProperty :: Property
-someProperty =
-  forAll (choose (5,10 :: Int)) (<= 10)
+testTypeInf = forAll genExpType $ \(e, t) ->
+  case inferTypeTop e of
+    Left err -> whenFail (prettyErrs (showExp e) err) (counterexample "Type inference failed!" False)
+    Right t' -> case unify [(t, fromScheme t', nowhere)] of
+                  Left err -> whenFail (prettyErrs (showExp e) err) (counterexample "Unification failed!" False)
+                  Right _  -> property True
+  where
+    fromScheme :: TypeScheme -> Type
+    fromScheme (Type t) = t
+    fromScheme (Forall _ t) = t
